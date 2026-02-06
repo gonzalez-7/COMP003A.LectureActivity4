198698d62072686ddacf76e4652f5bff613d46b2
gonzalez-7 <20agonzalezp@gmail.com>
2026-02-05 22:39:45 -0800

    Implement while loop for Section A task completion

--- COMP003A.LectureActivity4/Program.cs
+++ COMP003A.LectureActivity4/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("SECTION A = while Loop Practice");
+
+            // This loop completes tasks 1-5
+            int taskNumber = 1;
+
+            while (taskNumber <= 5)
+            {
+                Console.WriteLine("$Task {taskNumber} completed");
+                taskNumber++;
+            }
+            Console.WriteLine();
         }
     }
 }
